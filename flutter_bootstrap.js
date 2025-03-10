86cdb93a4470ac1af29d7ccfad35a06c65889f19
tamanna3v <tamanna3v@gmail.com>
2025-03-10 21:30:21 +0600

    finetuned

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"e672b006cb34c921db85b8e2f482ed3144a457
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "521328175"
+    serviceWorkerVersion: "3909284868"
   }
 });
