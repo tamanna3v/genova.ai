6a8e200a8f83bc8603393fff8a134fbf7c87f926
tamanna3v <tamanna3v@gmail.com>
2025-03-11 01:01:36 +0600

    prompt changed

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -24,7 +24,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "dc61a045d362f450d8bbd388ff893ae0
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "4b2350e14c6650ba82871f60906437ea",
-"flutter_bootstrap.js": "70bfa24c2a5811cb2549a28170ce694a",
+"flutter_bootstrap.js": "0b8ad87b0a663b352615d77f8d867f42",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
